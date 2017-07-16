b865108f4904fabec6ef5e48a56e5cf91e16d5c8
Yang Wang <mimimhmh@users.noreply.github.com>
2017-07-17 00:21:00 +1200

    reply avatar

--- resources/views/posts/post-comment.blade.php
+++ resources/views/posts/post-comment.blade.php
@@ -139,7 +139,7 @@
 
         let template = `
         <div class="blog-comment clearfix">
-            <a class="comment-avtar"><img src="{{URL::asset('${ data.avatar }')}}" alt="image"></a>
+            <a class="comment-avtar"><img src="${ data.avatar }" alt="image"></a>
                 <div class="comment-text">
                 <h3>${ data.user_name }</h3>
                 <h5>${ data.created_at }</h5>
