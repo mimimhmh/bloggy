5d1265fc543961821b077383c36eee67e279bd7d
Yang Wang <mimimhmh@users.noreply.github.com>
2017-07-17 01:16:57 +1200

    custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.full-master')
+
+@section('content')
+	<!-- Main -->
+	<div id="main">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="error-page text-align-center pad-top-big pad-bottom-big">
+						<h1>404</h1>
+						<h2>Oops, This Page Not Be Found!</h2>
+						<h3>We are really sorry but the page you requested is missing</h3>
+						<div class="error-bottom">
+							<a href="/" class="home-page-link">go to home page
+                                <i class="fa fa-angle-right" aria-hidden="true">
+                                </i>
+                            </a>
+						</div>
+					</div>
+				</div>
+			</div> <!-- End row -->
+		</div> <!-- End Container -->
+		<div id="instagram-footer">
+		</div>
+
+@endsection
